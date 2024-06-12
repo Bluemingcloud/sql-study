16a8c273199137b398088afb6ba07654d581abdb
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-12 18:09:07 +0900

    Add SQLBasic folder, SQLQuiz folder 2024 06 12

---
+++ SQLBasic/01SELECT.sql
@@ -0,0 +1,35 @@
+SELECT * FROM EMPLOYEES;
+SELECT * FROM DEPARTMENTS;
+SELECT * FROM JOBS;
+
+-- 특정 컬럼만 조회하기
+-- 문자와 날짜는 왼쪽에, 숫자는 오른쪽에 표시됩니다.
+SELECT FIRST_NAME, HIRE_DATE, EMAIL, SALARY FROM EMPLOYEES;
+
+-- 컬럼명 자리에서는 숫자 또는 날짜가 연산이 됩니다.
+SELECT FIRST_NAME, SALARY, SALARY + SALARY * 0.1 FROM EMPLOYEES;
+
+-- 컬럼 이름을 변경할 수 있습니다.
+-- 별칭
+-- AS 는 생략이 가능하지만 표기하는 것을 추천
+-- "" 로도 표기할 수 있지만 추천하지 않는다.
+SELECT FIRST_NAME AS 이름, SALARY AS 급여, SALARY + SALARY * 0.1 "최종급여" FROM EMPLOYEES;
+
+-- NULL 표기 확인
+-- PK 는 EMPLOYEE_ID, FK 는 DEPARTMENT_ID
+SELECT * FROM EMPLOYEES;
+
+-- 문자열의 표현
+-- 문자열 연결 ||, 문자열 추가 '', 문자열 안에 ' 사용은 '' 입력
+SELECT 'HELLO' || ' WORLD' FROM EMPLOYEES;
+SELECT FIRST_NAME || '님의 급여는 ' || SALARY || '$ 입니다' AS 급여 FROM EMPLOYEES;
+
+-- DISTINCT 중복제거 키워드
+-- SELECT 다음에 붙는다.
+SELECT DEPARTMENT_ID FROM EMPLOYEES;
+SELECT DISTINCT DEPARTMENT_ID FROM EMPLOYEES;
+
+-- ROWID 레코드가 저장된 위치 정보
+-- ROWNUM 조회된 순서
+SELECT EMPLOYEE_ID, FIRST_NAME, ROWID FROM EMPLOYEES;
+SELECT EMPLOYEE_ID, FIRST_NAME, ROWNUM FROM EMPLOYEES;
